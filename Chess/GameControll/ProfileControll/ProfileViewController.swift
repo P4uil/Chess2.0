905adbb6bb82ebfc91df1346af58f8fbfac3e9f2
Totskiy Pavel <tockiypav@icloud.com>
2024-04-02 00:01:30 +0500

    Profile view added

--- Chess/GameControll/ProfileControll/ProfileViewController.swift
+++ Chess/GameControll/ProfileControll/ProfileViewController.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import UIKit
+
+
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var playerNameTextField: UITextField!
+    @IBOutlet weak var achivements: UILabel!
+    @IBOutlet weak var achivementsTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    @IBAction func didTapSettingsButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapAddFriendButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+    }
+    
+    
+}
+
+class SettingsViewController: UIViewController {
+    
+}
